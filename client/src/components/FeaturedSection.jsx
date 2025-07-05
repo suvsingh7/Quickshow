fcd419db62e9dac477ff297958b6f51a0013be93
suvsingh7 <suvendras777@gmail.com>
2025-07-05 14:34:10 +0530

    padding issue fixed

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -12,7 +12,7 @@ const FeaturedSection = () => {
     
 
   return (
-    <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-38 overflow-hidden'>
 
         <div className='relative flex items-center justify-between pt-20 pb-10'>
             <BlurCircle top='0' right='-80px'/>
--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -5,7 +5,7 @@ import BlurCircle from '../components/BlurCircle'
 
 const Movies = () => {
   return dummyShowsData.length >0 ?(
-    <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden 
+    <div className='relative my-40 mb-60 px-6 md:px-20 lg:px-40 xl:px-38 overflow-hidden 
     min-h-[80vh]'>
       <BlurCircle top="150px" left="0px"/>
       <BlurCircle bottom="50px" right="50px"/>
