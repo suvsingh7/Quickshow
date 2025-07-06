49122986d4750b54f7375d18d55120dd8fb1cb8a
suvsingh7 <suvendras777@gmail.com>
2025-07-06 23:19:25 +0530

    Admin Panel-Navbar,sidebar,dashboard updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,11 @@ import MyBookings from './pages/MyBookings'
 import Favorite from './pages/Favorite'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
+import Layout from './pages/admin/Layout'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
+import Dashboard from './pages/admin/Dashboard'
 
 const App = () => {
 
@@ -25,6 +30,14 @@ const App = () => {
         <Route path="/movies/:id/:date" element={<SeatLayout/>} />
         <Route path="/my-bookings" element={<MyBookings/>} />
         <Route path="/favorite" element={<Favorite/>} />
+        {/* Admin routes can be added here */}
+        <Route path="/admin/*" element={<Layout/>} >
+          <Route index element={<Dashboard/>}/>
+          <Route path="add-shows" element={<AddShows/>}/>
+          <Route path="list-shows" element={<ListShows/>}/>
+          <Route path="list-bookings" element={<ListBookings/>}/>
+
+        </Route>
       </Routes>
        {!isAdminRoute && <Footer />}
     </>
