d6dba5ee30e9815a358d71765f3125cfcaa9de3e
suvsingh7 <suvendras777@gmail.com>
2025-07-07 11:53:34 +0530

    Frontend Completed

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -115,7 +115,30 @@ const AddShows = () => {
             </div>
         </div>
 
-       
+       {/* Display Selected Times */}
+               {Object.keys(dateTimeSelection).length > 0 && (
+               <div className="mt-6">
+                   <h2 className=" mb-2">Selected Date-Time</h2>
+                   <ul className="space-y-3">
+                       {Object.entries(dateTimeSelection).map(([date, times]) => (
+                           <li key={date}>
+                               <div className="font-medium">{date}</div>
+                               <div className="flex flex-wrap gap-2 mt-1 text-sm">
+                                   {times.map((time) => (
+                                       <div key={time} className="border border-primary px-2 py-1 flex items-center rounded" >
+                                           <span>{time}</span>
+                                           <DeleteIcon onClick={() => handleRemoveTime(date, time)} width={15} className="ml-2 text-red-500 hover:text-red-700 cursor-pointer" />
+                                       </div>
+                                   ))}
+                               </div>
+                           </li>
+                       ))}
+                   </ul>
+                   </div>
+              )}
+              <button  className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer" >
+                   Add Show
+               </button>
        
     </>
   ) : <Loading />
